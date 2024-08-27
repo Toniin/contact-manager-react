1216898a9824fc0723fd6c66b9e14f0218602638
Toniin <antonin.chaudiere@hotmail.fr>
2024-08-27 10:02:23 +0200

    First commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+// import ButtonSignOut from "@/components/buttons/ButtonSignOut.tsx";
+// import {useAppSelector} from "@/redux/hooks.ts";
+
+function Header() {
+    // const user = useAppSelector(state => state.persistedReducer)
+
+    return (
+        <header className="container flex justify-between items-center py-10">
+            <h1 className="text-5xl">Contact manager</h1>
+            {/*{user.isSignIn ?*/}
+            {/*    <ButtonSignOut/>*/}
+            {/*    : null}*/}
+        </header>
+    );
+}
+
+export default Header;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+import {
+    createBrowserRouter,
+    // redirect,
+    RouterProvider,
+} from "react-router-dom";
+import App from "./App";
+// import ContactsRoute from "@/routes/protected/ContactsRoute.tsx";
+// import AddContactRoute from "@/routes/protected/AddContactRoute.tsx";
+// import SignUpRoute from "@/routes/public/SignUpRoute.tsx";
+// import SignInRoute from "@/routes/public/SignInRoute.tsx";
+// import ProtectedRoutes from "@/routes/ProtectedRoutes.tsx";
+// import PublicRoutes from "@/routes/PublicRoutes.tsx";
+import ErrorRoute from "./routes/public/ErrorRoute";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        errorElement: <ErrorRoute />,
+        // children: [
+        //     {
+        //         index: true,
+        //         loader: async () => redirect('/sign-in'),
+        //     },
+        //     {
+        //         element: <PublicRoutes/>,
+        //         children: [
+        //             {
+        //                 path: "/sign-up",
+        //                 element: <SignUpRoute/>,
+        //             },
+        //             {
+        //                 path: "/sign-in",
+        //                 element: <SignInRoute/>,
+        //             },
+        //         ],
+        //     },
+        //     {
+        //         element: <ProtectedRoutes/>,
+        //         children: [
+        //             {
+        //                 path: "/contacts",
+        //                 element: <ContactsRoute/>,
+        //             },
+        //             {
+        //                 path: "/contacts/add",
+        //                 element: <AddContactRoute/>,
+        //             },
+        //         ],
+        //     },
+        // ],
+    },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
